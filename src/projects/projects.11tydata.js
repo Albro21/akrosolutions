e02edb332461e42841127faae2f941ded4013150
Albert <albert.kurto@gmail.com>
2025-12-19 22:41:34 +0200

    Replace projects.html with projects.njk and removed permalink from it, refactored projects.11tydata.js

--- src/projects/projects.11tydata.js
+++ src/projects/projects.11tydata.js
@@ -5,9 +5,7 @@ export default {
   eleventyComputed: {
     images: data => {
       const dir = path.join("src/assets/images/projects", data.page.fileSlug);
-
       if (!fs.existsSync(dir)) return [];
-
       return fs.readdirSync(dir)
         .filter(f => /\.(png|jpg|jpeg|webp)$/i.test(f))
         .sort();
